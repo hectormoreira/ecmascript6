b440c14d734ea0e87f5ae06376592b2c7addc697
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2021-03-01 22:25:17 -0500

    let, var, const

--- src/es6/index.js
+++ src/es6/index.js
@@ -21,3 +21,52 @@ console.log(epicPhrase);
 //Template literal
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+//Multilínea
+let lorem =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. \n" +
+  " At, voluptate laudantium! Corrupti quod sint aperiam beatae eius id debitis iusto fugit, consequuntur, reprehenderit minus, illo itaque explicabo nulla voluptate quaerat?";
+
+//es6
+let lorem2 = `Otra frase epica que necesitamos
+salto de linea
+`;
+console.log(lorem);
+console.log(lorem2);
+
+// Desestructuración
+let person = {
+  name: "Hector",
+  age: 32,
+  country: "EC",
+};
+console.log(person.name, person.age);
+
+//es6
+let { name } = person;
+console.log(name);
+
+
+//Spread Operator
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yessica', 'Camila'];
+
+let education = ['David', ...team1, ...team2 ];
+console.log(education);
+
+//es6
+//let > Disponible solo dentro del scope
+//var > Disponible global
+{
+    var globalVar = "Global Var";
+}
+
+{
+    let globalLet = "Gloval Let";
+    console.log(globalLet);
+}
+console.log(globalVar);
+
+//const > Constante que no cambia, no es posoble cambiar su valor
+const a = 'b';
+// no es posible a = 'c'
